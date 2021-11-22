4445fdf7b1a410733a19b50a1e399f3ef3da0004
gui <guilhermessecoortiz@gmail.com>
2021-11-22 16:26:12 +0000

    Save Audio Prefs
    - Audio Preferences
    - Naughty attributes

--- TimeDraggers(MOBILE)/Assets/Scripts/Audio/AudioManager.cs
+++ TimeDraggers(MOBILE)/Assets/Scripts/Audio/AudioManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using NaughtyAttributes;
 public class AudioManager : MonoBehaviour
 {
 
@@ -12,12 +13,34 @@ public class AudioManager : MonoBehaviour
         instance = this;
     }
     #endregion
+    [OnValueChanged("SavePrefs")]
+    public float MusicVolume = 50;
+    [OnValueChanged("SavePrefs")]
+    public float SfxVolume = 50;
 
-    public Sound[] sounds;
-    public void Play(string name,float volume = 50)
+    public Sound[] OSTs;
+    public Sound[] SFX;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("MusicVolume") || !PlayerPrefs.HasKey("SfxVolume"))
+        {
+            PlayerPrefs.SetFloat("MusicVolume", 50);
+            PlayerPrefs.SetFloat("SfxVolume", 50);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MusicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        SfxVolume = PlayerPrefs.GetFloat("SfxVolume");
+    }
+
+
+    public void Play(string name)
     {
 
-        Sound s = Array.Find(sounds, Sound => Sound.Name == name);
+        Sound s = Array.Find(OSTs, Sound => Sound.Name == name);
 
 
         if (s == null)
@@ -25,8 +48,34 @@ public class AudioManager : MonoBehaviour
             Debug.Log("\"" + name + "\"" + " could not be found");
             return;
         }
-        s.source.volume = volume;
+        s.source.volume = MusicVolume;
         s.source.pitch = s.pitch;
         s.source.Play();
+    }
+    public void PlaySfx(string name)
+    {
+
+        Sound s = Array.Find(SFX, Sound => Sound.Name == name);
+
+
+        if (s == null)
+        {
+            Debug.Log("\"" + name + "\"" + " could not be found");
+            return;
+        }
+        s.source.volume = SfxVolume;
+        s.source.pitch = s.pitch;
+        s.source.Play();
+    }
+
+    public void SavePrefs()
+    {
+        PlayerPrefs.SetFloat("MusicVolume", MusicVolume);
+        PlayerPrefs.SetFloat("SfxVolume", SfxVolume);
+    }
+    public void LoadPrefs()
+    {
+        MusicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        SfxVolume = PlayerPrefs.GetFloat("SfxVolume");
     }
 }
--- TimeDraggers(MOBILE)/Assets/Scripts/Menu.cs
+++ TimeDraggers(MOBILE)/Assets/Scripts/Menu.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using NaughtyAttributes;
 public class Menu : MonoBehaviour
 {
 
-    
-    
+    public bool isMainMenu;
+    [ShowIf("isMainMenu")]
+    public Slider Music;
+    [ShowIf("isMainMenu")]
+    public Slider SFX;
+
+
+    private void Start()
+    {
+        Music.onValueChanged.AddListener(delegate { UpdateAudio(); });
+        SFX.onValueChanged.AddListener(delegate { UpdateAudio(); });
+    }
+
+
     public void NewGame()
     {
         PlayerPrefs.DeleteKey("SavedLevel");
@@ -38,5 +51,12 @@ public class Menu : MonoBehaviour
     {
         Application.Quit();
     }
-
+    public void UpdateAudio()
+    {
+        if(isMainMenu)
+        {
+            AudioManager.instance.MusicVolume = Music.value;
+            AudioManager.instance.SfxVolume = SFX.value;
+        }
+    }
 }
